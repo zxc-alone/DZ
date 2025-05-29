6195844956478565f825e3d5bb1b9b75659e98df
zxc <ghg2.0@mail.ru>
2025-05-29 15:13:11 +0300

    Update test.js

--- task2/test.js
+++ task2/test.js
@@ -1,1 +1,6 @@
 
+const p = document.querySelector('p.text')
+console.log(p.previousElementSibling); // h2
+console.log(p.parentElement); // content
+console.log(document.querySelector('png')); // img
+console.log(p.parentElement.parentElement); // elem
