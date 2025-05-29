dd52ab44066b7d38e1154537cc34b061f2adc531
zxc <ghg2.0@mail.ru>
2025-05-29 15:12:21 +0300

    Update test.js

--- task1/test.js
+++ task1/test.js
@@ -1,1 +1,13 @@
+const blockDiv = document.querySelector('div.block');
+const newDiv = document.createElement('div');
+newDiv.className= 'item';
+newDiv.textContent='Элемент внутри';
 
+newDiv.style.color='blue';
+newDiv.style.border='solid black';
+newDiv.style.backgroundColor='#f8f8f8';
+newDiv.style.padding='16px';
+
+newDiv.setAttribute('class', 'item_1');
+
+blockDiv.appendChild(newDiv);
