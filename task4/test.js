0e6cf967bb20c43703a21d1b9558737a71df80b3
zxc <ghg2.0@mail.ru>
2025-05-29 15:16:39 +0300

    Update test.js

--- task4/test.js
+++ task4/test.js
@@ -1,1 +1,18 @@
+const btn = document.querySelector('.btn');
+const inp = document.querySelector(`input[type="text"]`);
 
+btn.addEventListener('click', ()=> {
+    if (inp.value.length == 0){
+        if (!document.querySelector('h2')){
+            const h2 = document.createElement('h2');
+            h2.textContent = 'Вы не заполнили поле ввода';
+            h2.style.color = 'red';
+            document.body.appendChild(h2);
+        }
+    }
+    else {
+        if (document.querySelector('h2')){
+            document.querySelector('h2').remove();
+        }
+    }
+})
