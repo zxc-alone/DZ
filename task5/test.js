9f507013742bd1c69b195190b62f9b984d434a4f
zxc <ghg2.0@mail.ru>
2025-05-29 15:17:24 +0300

    Update test.js

--- task5/test.js
+++ task5/test.js
@@ -1,1 +1,8 @@
+const content = document.querySelector('.content');
+const button = document.createElement('button');
+button.textContent='Отправить';
+content.appendChild(button);
 
+button.addEventListener('click', (event)=> {
+    event.target.textContent='Текст отправлен';
+})
